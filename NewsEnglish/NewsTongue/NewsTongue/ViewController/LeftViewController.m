db54fe8314f256df8c34248eee3f6d1846da5fe3
ramonqlee1980 <ramonqlee1980@gmail.com>
2015-03-11 06:57:13 +0800

    左边栏增加单词收藏，新闻收藏和翻译助手占位

--- NewsEnglish/NewsTongue/NewsTongue/ViewController/LeftViewController.m
+++ NewsEnglish/NewsTongue/NewsTongue/ViewController/LeftViewController.m
@@ -19,7 +19,8 @@
 
 - (void)viewDidLoad
 {
-    _arData = @[@"收藏", @"翻译助手"];
+    _arData = @[@"单词收藏",@"新闻收藏", @"翻译助手"];
+    SEL selectors[] = {@selector(wordList:),@selector(newsList::),@selector(translator:)};
     
     [self.view setBackgroundColor:[UIColor clearColor]];
     UIImageView *imageBgV = [[UIImageView alloc] initWithFrame:self.view.bounds];
@@ -28,8 +29,9 @@
     
     __block float h = self.view.frame.size.height*0.7/[_arData count];
     __block float y = 0.15*self.view.frame.size.height;
-    [_arData enumerateObjectsUsingBlock:^(NSString *obj, NSUInteger idx, BOOL *stop)
+    for (NSInteger i = 0; i < _arData.count; ++i)
     {
+        NSString* obj = [_arData objectAtIndex:i];
         UIView *listV = [[UIView alloc] initWithFrame:CGRectMake(0, y, self.view.frame.size.width, h)];
         [listV setBackgroundColor:[UIColor clearColor]];
         UILabel *l = [[UILabel alloc] initWithFrame:CGRectMake(60, 0, listV.frame.size.width - 60, listV.frame.size.height)];
@@ -40,7 +42,23 @@
         [listV addSubview:l];
         [self.view addSubview:listV];
         y += h;
-    }];
+    };
 }
 
+#pragma mark selectors
+-(void)wordList:(UIView*)view
+{
+    
+}
+
+-(void)newsList:(UIView*)view
+{
+    
+}
+
+
+-(void)translator:(UIView*)view
+{
+    
+}
 @end
